498a836ff883a9d14191ffa7c9ab776b1a8a2199
Trystan Kaes <baby.hughee@gmail.com>
2020-03-04 22:57:10 -0700

    Minor changes. made response time customizable

--- IRC_BOT/bot.py
+++ IRC_BOT/bot.py
@@ -7,6 +7,8 @@ import irc
 import workers
 import functions as func
 
+RESPONSE_TIME = 0.01
+MSG_PER_MINUTE = 2
 
 #################### IRC CONFIG ####################
 PORT = 6666
@@ -26,7 +28,7 @@ IRC.send(CHANNEL, f"{BOTNICK} logging on")
 ################ Print Chat Connection Info ################
 RESPONSE = ""
 while RESPONSE.find(f"JOIN {CHANNEL}") < 0:
-    RESPONSE = IRC.get_response()
+    RESPONSE = IRC.get_response().rstrip()
     print(RESPONSE + "\n")
 ################ Print Chat Connection Info ################
 
@@ -34,30 +36,30 @@ while RESPONSE.find(f"JOIN {CHANNEL}") < 0:
 
 ################ WORKER INITIALIZATION ################
 MESSAGE_CACHE = [] # Initialize the autotracking message queue
-CLEANER = workers.MessageQueueCleaner(1, MESSAGE_CACHE)
+CLEANER = workers.MessageQueueCleaner(RESPONSE_TIME, MESSAGE_CACHE)
 TALKER_DICT = {} # Initialize talker dictionary
-TRACKER = workers.UserTracker(2, MESSAGE_CACHE, TALKER_DICT)
+TRACKER = workers.UserTracker(RESPONSE_TIME, MESSAGE_CACHE, TALKER_DICT)
 ################ WORKER INITIALIZATION ################
 
 BANNED_WORDS = func.read_data("BANNED_WORDS")
 
 ################ MAIN PROCESS EXECUTION ################
 while True:
-    RESPONSE = IRC.get_response()
+    RESPONSE = IRC.get_response().rstrip()
     print(RESPONSE)
 
     # Check for swearing
     if func.test_match(BANNED_WORDS, RESPONSE):
         BAD_USER = re.findall(r'(?<=\:)(.*?)(?=\!)', RESPONSE) #extract username
         if len(BAD_USER) > 0:
-            IRC.send(CHANNEL, f"clean it up @{BAD_USER[0]}!")
+            IRC.send(CHANNEL, f"Clean it up @{BAD_USER[0]}!")
 
     # Check for spam
-    if func.test_max(TALKER_DICT, 5):
+    if func.test_max(TALKER_DICT, MSG_PER_MINUTE):
         BAD_USER = re.findall(r'(?<=\:)(.*?)(?=\!)', RESPONSE) #extract username
         if len(BAD_USER) > 0:
-            IRC.send(CHANNEL, f"Settle Down @{BAD_USER[0]}!")
+            IRC.send(CHANNEL, f"Take about 20% off there @{BAD_USER[0]}!")
 
-        #store message in cache
+    #store message in cache
     MESSAGE_CACHE.append([time.process_time(), RESPONSE.rstrip()])
 ################ MAIN PROCESS EXECUTION ################
